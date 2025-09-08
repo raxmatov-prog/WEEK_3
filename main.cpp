537691030a19f1b681828ca59dcf7a9573126b53
Normo'min <nor.raxmatov@newuu.uz>
2025-09-08 14:32:54 +0500

    I almost solved 4 problem

---
+++ main.cpp
@@ -0,0 +1,63 @@
+# include <iostream>
+using namespace std;
+
+//Problem 1:
+/*int main() {
+    int intNumber = 24;
+    float floatNumber = 3.14;
+    double doubleNumber = 45.1234;
+    bool boolean = true;
+    char charNumber = 'A';
+    cout << "Value of integer is "<< intNumber << ", Size is " << sizeof(intNumber) << endl;
+    cout << "Value of float is " << floatNumber << ", Size is " << sizeof(floatNumber) << endl;
+    cout << "Value of double is " << doubleNumber << ", Size is " << sizeof(doubleNumber) << endl;
+    cout << "Value of bool is " << boolean << ", Size is " << sizeof(boolean) << endl;
+    cout << "Value of char is " << charNumber << ", Size is " << sizeof(boolean) << endl;
+    return 0;
+}
+
+
+//Problem 2:
+
+int main()
+{
+  string name = "Alijon Aliev";
+    int id = 233242;
+    string song = "Queen - The Show Must Go On";
+    string movie = "Titanic";
+    cout << name << endl;
+    cout << id << endl;
+    cout << song << endl;
+    cout << movie << endl;
+    return 0;
+}
+
+
+//Problem 3:
+
+int main()
+{
+ cout << "\t \"I\'m standing\" on the edge of some crazy cliff." << "\n";
+    cout << "\t What I have to do\," << "I have to catch everybody\\" << "\n";
+    cout << "\t if they start to go over the cliff\-" << "I mean\\" << "\n";
+    cout << "\t \\if they’re running and they don\'t look" << "\n";
+    cout << "\t where they\'re going I have to come out" << "\n";
+    cout << "\t from somewhere and\t\t catch them.\n";
+    cout << "Refer to escape sequences table." << endl;
+    return 0;
+}
+*/
+
+//Problem 4: :
+
+int main()
+{
+    int a = 189;
+    char b = 'B';
+    float d = a/3;
+    int decimal_Value_of_b = b;
+    cin >> a, b = 'B';
+    cout << d;
+
+    return 0;
+}
